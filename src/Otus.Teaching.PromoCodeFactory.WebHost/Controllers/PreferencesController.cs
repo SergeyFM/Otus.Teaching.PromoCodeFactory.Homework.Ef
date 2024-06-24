c4b9f1278364ab3da5207485de69ecaac382f679
SergeyFM <github@svistunov.su>
2024-06-24 16:41:42 +0300

    Added PreferencesController

---
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Controllers/PreferencesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Otus.Teaching.PromoCodeFactory.Core.Abstractions.Repositories;
+using Otus.Teaching.PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using Otus.Teaching.PromoCodeFactory.WebHost.Models;
+
+namespace Otus.Teaching.PromoCodeFactory.WebHost.Controllers;
+
+/// <summary>
+/// Контроллер для получения списка предпочтений
+/// </summary>
+[ApiController]
+[Route("api/v1/[controller]")]
+public class PreferencesController(
+    IRepository<Preference> _preferenceRepository
+    ) : ControllerBase {
+
+    /// <summary>
+    /// Получение списка предпочтений
+    /// </summary>
+    /// <returns>Список предпочтений</returns>
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PreferenceResponse>>> GetPreferencesAsync() {
+        IEnumerable<Preference> preferences = await _preferenceRepository.GetAllAsync();
+        List<PreferenceResponse> response = preferences.Select(p => new PreferenceResponse {
+            Id = p.Id,
+            Name = p.Name
+        }).ToList();
+
+        return Ok(response);
+    }
+}
